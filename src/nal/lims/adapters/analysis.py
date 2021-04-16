b519cc6c604914c20bdc27d8df7f010865f1f622
Paul <PVanderWeele@newagelaboratories.com>
2021-04-16 18:41:33 -0400

    Moved Analysis Date/Time to nal.lims addon

---
+++ src/nal/lims/adapters/analysis.py
@@ -0,0 +1,45 @@
+from bika.lims.interfaces import IAnalysis
+from Products.ATContentTypes.content import schemata
+from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
+from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
+from archetypes.schemaextender.interfaces import ISchemaModifier
+from bika.lims.fields import ExtStringField
+from bika.lims import bikaMessageFactory as _
+from zope.component import adapts
+from zope.interface import implements
+from nal.lims.interfaces import INalLimsLayer
+from Products.Archetypes.public import StringWidget
+from Products.CMFCore.permissions import View
+from Products.Archetypes.Schema import Schema
+
+class AnalysisSchemaExtender(object):
+    adapts(IAnalysis)
+    implements(IOrderableSchemaExtender, IBrowserLayerAwareExtender)
+    layer = INalLimsLayer
+
+    fields = [
+        ExtStringField(
+            'AnalysisDateTime',
+            read_permission=View,
+            write_permission="Field: Edit Result",
+        )
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getOrder(self, schematas):
+        return schematas
+
+    def getFields(self):
+        return self.fields
+
+class AnalysisSchemaModifier(object):
+    adapts(IAnalysis)
+    implements(ISchemaModifier)
+
+    def __init__(self, context):
+        self.context = context
+
+    def fiddle(self, schema):
+        return schema
