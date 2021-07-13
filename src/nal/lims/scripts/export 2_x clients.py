ba0683592fa8622f59cfd078565fdb02309dfd03
Paul <faytro@hotmail.com>
2021-07-13 17:44:17 -0400

    Added Status column

--- src/nal/lims/scripts/export 2_x clients.py
+++ src/nal/lims/scripts/export 2_x clients.py
@@ -8,27 +8,43 @@ me = UnrestrictedUser(getSecurityManager().getUser().getUserName(), '', ['LabMan
 me = me.__of__(portal.acl_users)
 newSecurityManager(None, me)
 
-clients = map(api.get_object, api.search({'portal_type':'Client', "inactive_status": "active"}))
+clients = map(api.get_object, api.search({'portal_type':'Client'}))
 data = []
-cols = ['NAL Number','Client ID','Email Address','Phone','MBG Grower Number','TrueBlue Grower Number','Grower List','Country','State','District','City','Zip','Address']
+cols = ['Status',
+        'NAL Number',
+        'Client ID',
+        'Email Address',
+        'Phone',
+        'MBG Grower Number',
+        'TrueBlue Grower Number',
+        'Grower List',
+        'Country',
+        'State',
+        'District',
+        'City',
+        'Zip',
+        'Address',
+        
+]
 for i in clients:
     thisclient = {}
-    thisclient[cols[0]] = i.ClientID
-    thisclient[cols[1]] = i.Name
-    thisclient[cols[2]] = i.EmailAddress
-    thisclient[cols[3]] = i.Phone
-    thisclient[cols[4]] = i.MBGGrowerNumber
-    thisclient[cols[5]] = i.TBGrowerNumber
+    thisclient[cols[0]] = api.get_workflow_status_of(i)
+    thisclient[cols[1]] = i.ClientID
+    thisclient[cols[2]] = i.Name
+    thisclient[cols[3]] = i.EmailAddress
+    thisclient[cols[4]] = i.Phone
+    thisclient[cols[5]] = i.MBGGrowerNumber
+    thisclient[cols[6]] = i.TBGrowerNumber
     distributor_client_numbers = []
     for j in i.getReferences(relationship="ClientDistributor"):
         distributor_client_numbers.append(j.ClientID)
-    thisclient[cols[6]] = ";".join(distributor_client_numbers)
-    thisclient[cols[7]] = i.PhysicalAddress['country']
-    thisclient[cols[8]] = i.PhysicalAddress['state']
-    thisclient[cols[9]] = i.PhysicalAddress['district']
-    thisclient[cols[10]] = i.PhysicalAddress['city']
-    thisclient[cols[11]] = i.PhysicalAddress['zip']
-    thisclient[cols[12]] = i.PhysicalAddress['address']
+    thisclient[cols[7]] = ";".join(distributor_client_numbers)
+    thisclient[cols[8]] = i.PhysicalAddress['country']
+    thisclient[cols[9]] = i.PhysicalAddress['state']
+    thisclient[cols[10]] = i.PhysicalAddress['district']
+    thisclient[cols[11]] = i.PhysicalAddress['city']
+    thisclient[cols[12]] = i.PhysicalAddress['zip']
+    thisclient[cols[13]] = i.PhysicalAddress['address']
     data.append(thisclient)
 
 try:
