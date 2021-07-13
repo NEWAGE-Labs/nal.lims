2bd4cd37296cc548ba1b0f4b97eb9294ec849456
Paul <faytro@hotmail.com>
2021-07-13 17:53:06 -0400

    Removed 'Active search, and Added Status and Client Status.

--- src/nal/lims/scripts/export 2_x sdgs.py
+++ src/nal/lims/scripts/export 2_x sdgs.py
@@ -8,22 +8,50 @@ me = UnrestrictedUser(getSecurityManager().getUser().getUserName(), '', ['LabMan
 me = me.__of__(portal.acl_users)
 newSecurityManager(None, me)
 
-sdgs = map(api.get_object, api.search({'portal_type':'Batch', "inactive_status": "active"}))
+sdgs = map(api.get_object, api.search({'portal_type':'Batch'}))
 data = []
-cols = ['Batch ID', 'ProjectName', 'Client Number', 'Client Name', 'SDG Received Date', 'SDG Received Time', 'Project Contact', 'Sampled By', 'Report Contact', 'Labels', 'Notes']
+cols = ['Status',
+        'Batch ID',
+        'ProjectName',
+        'Client Number',
+        'Client Name',
+        'Client Status',
+        'SDG Received Date',
+        'SDG Received Time',
+        'Project Contact',
+        'Sampled By',
+        'Report Contact',
+        'Labels',
+        'Notes',
+
+]
+
 for i in sdgs:
     thissdg = {}
-    thissdg[cols[0]] = i.id
-    thissdg[cols[1]] = i.title
-    thissdg[cols[2]] = i.getClient().ClientID
-    thissdg[cols[3]] = i.getClient().Name
-    thissdg[cols[4]] = i.SDGDate.Date()
-    thissdg[cols[5]] = i.SDGTime
-    thissdg[cols[6]] = i.getReferences(relationship="SDGProjectContact")[0].getFullname()
-    thissdg[cols[7]] = i.getReferences(relationship="SDGSamplerContact")[0].getFullname()
-    thissdg[cols[8]] = i.ReportContact
-    thissdg[cols[9]] = ";".join(i.getLabelNames())
-    thissdg[cols[10]] = i['description']
+    thissdg[cols[0]] = api.get_workflow_status_of(i)
+    thissdg[cols[1]] = i.id
+    thissdg[cols[2]] = i.title
+    client = i.getClient()
+    if client:
+        try:
+            thissdg[cols[3]] = client.ClientID
+        except AttributeError:
+            thissdg[cols[3]] = ''
+        try:
+            thissdg[cols[4]] = client.Name
+        except AttributeError:
+            thissdg[cols[4]] = ''
+        try:
+            thissdg[cols[5]] = api.get_workflow_status_of(client)
+        except AttributeError:
+            thissdg[cols[5]] = ''
+    thissdg[cols[6]] = i.SDGDate.Date()
+    thissdg[cols[7]] = i.SDGTime
+    thissdg[cols[8]] = i.getReferences(relationship="SDGProjectContact")[0].getFullname()
+    thissdg[cols[9]] = i.getReferences(relationship="SDGSamplerContact")[0].getFullname()
+    thissdg[cols[10]] = i.ReportContact
+    thissdg[cols[11]] = ";".join(i.getLabelNames())
+    thissdg[cols[12]] = i['description']
     data.append(thissdg)
 
 try:
