c45f36a46ced5d112cd2b657117f24d73fb1c456
Paul <faytro@hotmail.com>
2021-07-20 16:58:51 -0400

    Added logic to client import

--- src/nal/lims/scripts/import 2_x clients.py
+++ src/nal/lims/scripts/import 2_x clients.py
@@ -8,54 +8,64 @@ portal = api.get_portal()
 me = UnrestrictedUser(getSecurityManager().getUser().getUserName(), '', ['LabManager'], '')
 me = me.__of__(portal.acl_users)
 newSecurityManager(None, me)
-#
-# clients = map(api.get_object, api.search({'portal_type':'Client'}))
-# data = []
-# cols = ['Status',
-#         'NAL Number',
-#         'Client ID',
-#         'Email Address',
-#         'Phone',
-#         'MBG Grower Number',
-#         'TrueBlue Grower Number',
-#         'Grower List',
-#         'Country',
-#         'State',
-#         'District',
-#         'City',
-#         'Zip',
-#         'Address',
+
+clients = pd.read_csv('migration data/clients_export.csv')
+
+#Needs Status added in
+# raw_cols = ['NAL Number',
+#             'Client Name',
+#             'Email Address',
+#             'Phone',
+#             'Grower Number',
+#             'Country',
+#             'State',
+#             'District',
+#             'City',
+#             'Zip',
+#             'Address',
 #
 # ]
-# for i in clients:
-#     thisclient = {}
-#     thisclient[cols[0]] = api.get_workflow_status_of(i)
-#     thisclient[cols[1]] = i.ClientID
-#     thisclient[cols[2]] = i.Name
-#     thisclient[cols[3]] = i.EmailAddress
-#     thisclient[cols[4]] = i.Phone
-#     thisclient[cols[5]] = i.MBGGrowerNumber
-#     thisclient[cols[6]] = i.TBGrowerNumber
-#     distributor_client_numbers = []
-#     for j in i.getReferences(relationship="ClientDistributor"):
-#         distributor_client_numbers.append(j.ClientID)
-#     thisclient[cols[7]] = ";".join(distributor_client_numbers)
-#     thisclient[cols[8]] = i.PhysicalAddress['country']
-#     thisclient[cols[9]] = i.PhysicalAddress['state']
-#     thisclient[cols[10]] = i.PhysicalAddress['district']
-#     thisclient[cols[11]] = i.PhysicalAddress['city']
-#     thisclient[cols[12]] = i.PhysicalAddress['zip']
-#     thisclient[cols[13]] = i.PhysicalAddress['address']
-#     data.append(thisclient)
 #
-# try:
-#     with open("clients_export.csv", 'w') as csvfile:
-#         writer = csv.DictWriter(csvfile, fieldnames=cols)
-#         writer.writeheader()
-#         for aclient in data:
-#             try:
-#                 writer.writerow(aclient)
-#             except UnicodeEncodeError:
-#                 print(aclient)
-# except IOError:
-#     print("I/O Error")
+# clean_cols = [  'Status',
+#                 'NAL Number',
+#                 'Client ID',
+#                 'Email Address',
+#                 'Phone',
+#                 'MBG Grower Number',
+#                 'TrueBlue Grower Number',
+#                 'Grower List',
+#                 'Country',
+#                 'State',
+#                 'District',
+#                 'City',
+#                 'Zip',
+#                 'Address',
+#
+# ]
+for i, row in clients.itterrows():
+    thisclient = api.create( portal.clients,
+                "Client",
+                ClientID=row['NAL Number'],
+                EmailAddress=row['Email Address'],
+                Phone=row['Phone'],
+                MBGGrowerNumber=row['Grower Number'],
+                PhysicalAddress['country']=row['Country'],
+                PostalAddress['country']=row['Country'],
+                BillingAddress['country']=row['Country'],
+                PhysicalAddress['state']=row['State'],
+                PostalAddress['state']=row['State'],
+                BillingAddress['state']=row['State'],
+                PhysicalAddress['district']=row['District'],
+                PostalAddress['district']=row['District'],
+                BillingAddress['district']=row['District'],
+                PhysicalAddress['city']=row['City'],
+                PostalAddress['city']=row['City'],
+                BillingAddress['city']=row['City'],
+                PhysicalAddress['zip']=row['Zip'],
+                PostalAddress['zip']=row['Zip'],
+                BillingAddress['zip']=row['Zip'],
+                PhysicalAddress['address']=row['Address'],
+                PostalAddress['address']=row['Address'],
+                BillingAddress['address']=row['Address']
+    )
+    thisclient.reindexObject()
