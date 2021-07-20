e5123cc7ee14c2af065d3655aa83309b0054d63a
Paul <faytro@hotmail.com>
2021-07-20 15:10:22 -0400

    Added skeleton for import scripts

---
+++ src/nal/lims/scripts/import 2_x contacts.py
@@ -0,0 +1,64 @@
+from AccessControl import getSecurityManager
+from AccessControl.User import UnrestrictedUser
+from AccessControl.SecurityManagement import newSecurityManager
+from bika.lims import api
+import csv
+portal = api.get_portal()
+me = UnrestrictedUser(getSecurityManager().getUser().getUserName(), '', ['LabManager'], '')
+me = me.__of__(portal.acl_users)
+newSecurityManager(None, me)
+#
+# contacts = map(api.get_object, api.search({'portal_type':'Contact'}))
+# data = []
+# cols = ['Status',
+#         'Client Number',
+#         'Client Name',
+#         'Client Status',
+#         'First Name',
+#         'Middle Name',
+#         'Surname',
+#         'Initials',
+#         'Email Address',
+#         'Business Phone',
+#         'Home Phone',
+#         'Mobile Phone',
+#
+# ]
+# for i in contacts:
+#     thiscontact = {}
+#     thiscontact[cols[0]] = api.get_workflow_status_of(i)
+#     client = api.get_parent(i)
+#     if client:
+#         try:
+#             thiscontact[cols[1]] = client.ClientID
+#         except AttributeError:
+#             thiscontact[cols[1]] = ''
+#         try:
+#             thiscontact[cols[2]] = client.Name
+#         except AttributeError:
+#             thiscontact[cols[2]] = ''
+#         try:
+#             thiscontact[cols[3]] = api.get_workflow_status_of(client)
+#         except AttributeError:
+#             thiscontact[cols[3]] = ''
+#     thiscontact[cols[4]] = i.Firstname
+#     thiscontact[cols[5]] = i.Middlename
+#     thiscontact[cols[6]] = i.Surname
+#     thiscontact[cols[7]] = i.Initials
+#     thiscontact[cols[8]] = i.EmailAddress
+#     thiscontact[cols[9]] = i.BusinessPhone
+#     thiscontact[cols[10]] = i.HomePhone
+#     thiscontact[cols[11]] = i.MobilePhone
+#     data.append(thiscontact)
+#
+# try:
+#     with open("contacts_export.csv", 'w') as csvfile:
+#         writer = csv.DictWriter(csvfile, fieldnames=cols)
+#         writer.writeheader()
+#         for acontact in data:
+#             try:
+#                 writer.writerow(acontact)
+#             except UnicodeEncodeError:
+#                 print(acontact)
+# except IOError:
+#     print("I/O Error")
---
+++ src/nal/lims/scripts/import 2_x sdgs.py
@@ -0,0 +1,67 @@
+from AccessControl import getSecurityManager
+from AccessControl.User import UnrestrictedUser
+from AccessControl.SecurityManagement import newSecurityManager
+from bika.lims import api
+import csv
+portal = api.get_portal()
+me = UnrestrictedUser(getSecurityManager().getUser().getUserName(), '', ['LabManager'], '')
+me = me.__of__(portal.acl_users)
+newSecurityManager(None, me)
+#
+# sdgs = map(api.get_object, api.search({'portal_type':'Batch'}))
+# data = []
+# cols = ['Status',
+#         'Batch ID',
+#         'ProjectName',
+#         'Client Number',
+#         'Client Name',
+#         'Client Status',
+#         'SDG Received Date',
+#         'SDG Received Time',
+#         'Project Contact',
+#         'Sampled By',
+#         'Report Contact',
+#         'Labels',
+#         'Notes',
+#
+# ]
+#
+# for i in sdgs:
+#     thissdg = {}
+#     thissdg[cols[0]] = api.get_workflow_status_of(i)
+#     thissdg[cols[1]] = i.id
+#     thissdg[cols[2]] = i.title
+#     client = i.getClient()
+#     if client:
+#         try:
+#             thissdg[cols[3]] = client.ClientID
+#         except AttributeError:
+#             thissdg[cols[3]] = ''
+#         try:
+#             thissdg[cols[4]] = client.Name
+#         except AttributeError:
+#             thissdg[cols[4]] = ''
+#         try:
+#             thissdg[cols[5]] = api.get_workflow_status_of(client)
+#         except AttributeError:
+#             thissdg[cols[5]] = ''
+#     thissdg[cols[6]] = i.SDGDate.Date()
+#     thissdg[cols[7]] = i.SDGTime
+#     thissdg[cols[8]] = i.getReferences(relationship="SDGProjectContact")[0].getFullname()
+#     thissdg[cols[9]] = i.getReferences(relationship="SDGSamplerContact")[0].getFullname()
+#     thissdg[cols[10]] = i.ReportContact
+#     thissdg[cols[11]] = ";".join(i.getLabelNames())
+#     thissdg[cols[12]] = i['description']
+#     data.append(thissdg)
+#
+# try:
+#     with open("sdg_export.csv", 'w') as csvfile:
+#         writer = csv.DictWriter(csvfile, fieldnames=cols)
+#         writer.writeheader()
+#         for asdg in data:
+#             try:
+#                 writer.writerow(asdg)
+#             except UnicodeEncodeError:
+#                 print(asdg)
+# except IOError:
+#     print("I/O Error")
