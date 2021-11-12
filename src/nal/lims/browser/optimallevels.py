4dcf37f0dc6c05aa93d586917718b8669eebdfd3
Paul <faytro@hotmail.com>
2021-11-12 13:01:19 -0500

    Added Optimal Level view for applying optimal levels to all samples

---
+++ src/nal/lims/browser/optimallevels.py
@@ -0,0 +1,27 @@
+import collections
+
+from bika.lims import api
+from bika.lims import bikaMessageFactory as _
+from Products.Five.browser import BrowserView
+from Products.statusmessages.interfaces import IStatusMessage
+
+class OptimalLevelView(BrowserView):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+
+        ars = self.getAnalysisRequests()
+        ol = ars[0].getSpecification()
+
+        for i in ars:
+            i.Specification = ol
+            i.reindexObject(idxs=['Specification'])
+
+        IStatusMessage(self.request).addStatusMessage(
+                u"Successfully applied OLs to all samples".format(self.context.title, filepath)
+            )
+
+        self.request.response.redirect(api.get_url(self.context))
