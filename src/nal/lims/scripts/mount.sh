82fe77971ed0d57716cca02538362b08f3b0e9af
Paul VanderWeele <faytro@hotmail.com>
2022-06-14 17:24:24 -0400

    Update mount.sh
    Updated to avoid DNS

--- src/nal/lims/scripts/mount.sh
+++ src/nal/lims/scripts/mount.sh
@@ -1,2 +1,2 @@
 
-sudo mount -t cifs -o username=pvanderweele,domain=newagelab,uid=naladmin,gid=naladmin,dir_mode=0777,file_mode=0777 //NEWAGESERVER/Data /mnt/Data
+sudo mount -t cifs -o username=pvanderweele,domain=newagelab,uid=naladmin,gid=naladmin,dir_mode=0777,file_mode=0777 //10.1.10.2/Data /Mnt/Data
