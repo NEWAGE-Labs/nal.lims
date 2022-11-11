cb3c840a0226165c73accf28e5372c11f702c29e
Paul VanderWeele <faytro@hotmail.com>
2022-11-11 16:06:14 -0500

    Added Datamigration file

---
+++ src/nal/lims/datamigration.py
@@ -0,0 +1,228 @@
+
+from bika.lims import api
+import pandas as pd
+
+def get_clients_as_df():
+    """
+    :return: Returns a DataFrame of active Clients. Each row is a Client \
+    Each column is a Field.
+    :rtype: pandas.DataFrame
+
+    :param None: No Parameters
+
+    **Columns**:
+    - Column list is define inside the local variable 'cols'
+    """
+    clients = api.search({'portal_type':'Client'})
+    cols = [
+        #Default
+        'id',
+        'Name',
+        'NAL Number',
+        'Phone',
+        'Fax',
+        'Email',
+        'CSV',
+        #Addresses
+        ## Physical Address
+        'PhysCountry',
+        'PhysState',
+        'PhysDistrict',
+        'PhysCity',
+        'PhysPostal',
+        'PhysAddress',
+        ## Postal Address
+        'PostCountry',
+        'PostState',
+        'PostDistrict',
+        'PostCity',
+        'PostPostal',
+        'PostAddress',
+        ## Billing Address
+        'BillCountry',
+        'BillState',
+        'BillDistrict',
+        'BillCity',
+        'BillPostal',
+        'BillAddress',
+        #Bank details
+        'Bank Account Type',
+        'Bank Account Name',
+        'Bank Account Number',
+        'Bank Name',
+        'Bank Branch',
+        #Grower info
+        'MBGNumber',
+        'TrueBlueNumber',
+    ]
+    client_dict = {}
+    for i in cols:
+        client_dict[i] = []
+
+    for i in clients:
+        if api.get_workflow_status_of(i) == 'active':
+            client = api.get_object(i)
+            #Default
+            client_dict['id'].append(client.id) #Required
+            client_dict['Name'].append(client.Name) #Required
+            client_dict['NAL Number'].append(client.ClientID) #Required
+            client_dict['Phone'].append(client.Phone or '')
+            client_dict['Fax'].append(client.Fax or '')
+            client_dict['Email'].append(client.EmailAddress or '')
+            client_dict['CSV'].append(client.CSV or '')
+            #Addresses
+            ## Physical Address
+            client_dict['PhysCountry'].append(client.PhysicalAddress.get('country',''))
+            client_dict['PhysState'].append(client.PhysicalAddress.get('state',''))
+            client_dict['PhysDistrict'].append(client.PhysicalAddress.get('district',''))
+            client_dict['PhysCity'].append(client.PhysicalAddress.get('city',''))
+            client_dict['PhysPostal'].append(client.PhysicalAddress.get('zip',''))
+            client_dict['PhysAddress'].append(client.PhysicalAddress.get('address',''))
+            ## Postal Address
+            client_dict['PostCountry'].append(client.PostalAddress.get('country',''))
+            client_dict['PostState'].append(client.PostalAddress.get('state',''))
+            client_dict['PostDistrict'].append(client.PostalAddress.get('district',''))
+            client_dict['PostCity'].append(client.PostalAddress.get('city',''))
+            client_dict['PostPostal'].append(client.PostalAddress.get('zip',''))
+            client_dict['PostAddress'].append(client.PostalAddress.get('address',''))
+            ## Billing Address
+            client_dict['BillCountry'].append(client.BillingAddress.get('country',''))
+            client_dict['BillState'].append(client.BillingAddress.get('state',''))
+            client_dict['BillDistrict'].append(client.BillingAddress.get('district',''))
+            client_dict['BillCity'].append(client.BillingAddress.get('city',''))
+            client_dict['BillPostal'].append(client.BillingAddress.get('zip',''))
+            client_dict['BillAddress'].append(client.BillingAddress.get('address',''))
+            #Bank details
+            client_dict['Bank Account Type'].append(client.AccountType or '')
+            client_dict['Bank Account Name'].append(client.AccountName or '')
+            client_dict['Bank Account Number'].append(client.AccountNumber or '')
+            client_dict['Bank Name'].append(client.BankName or '')
+            client_dict['Bank Branch'].append(client.BankBranch or '')
+            #Grower info
+            client_dict['MBGNumber'].append(client.MBGGrowerNumber or '')
+            client_dict['TrueBlueNumber'].append(client.TBGrowerNumber or '')
+
+    return pd.DataFrame(client_dict)[cols]
+
+def get_labcontacts_as_df():
+    """
+    :return: Returns a DataFrame of active LabContacts
+    :rtype: DataFrame
+    """
+    labcontacts = api.search({'portal_type':'LabContact'})
+    cols = [
+        'Firsname',
+        'Surname',
+        'Initials',
+        'EmailAddress',
+    ]
+
+    labcontact_dict = {}
+    for i in labcontact_dict:
+        labcontact_dict[i] = []
+
+    for i in labcontacts:
+        if api.get_workflow_status_of(i) == 'active':
+            labcontact = api.get_object(i)
+            labcontact_dict['Firstname'].append(labcontact.Firstname) #Required
+            labcontact_dict['Surname'].append(labcontact.Surname) #Required
+            labcontact_dict['Initials'].append(labcontact.Initials or '')
+            labcontact_dict['EmailAddress'].append(labcontact.EmailAddress or '')
+
+    return pd.DataFrame(labcontact_dict)[cols]
+
+def get_methods_as_df():
+    """
+    :return: Returns a DataFrame of active Methods
+    :rtype: DataFrame
+    """
+
+    methods = api.search({'portal_type':"Methods"})
+    cols = [
+        'title',
+        'description',
+        'accredited',
+    ]
+
+    method_dict = {}
+    for i in method_dict:
+        method_dict[i] = []
+
+    for i in methods:
+        if api.get_workflow_status_of(i) == 'active':
+            method = api.get_object(i)
+            method_dict['title'].append(method.title) #Required
+            method_dict['description'].append(method.description or '')
+            method_dict['accredited'].append(method.Accredited or '')
+
+    return pd.DataFrame(method_dict)[cols]
+
+def get_sdglabels_as_df():
+    """
+    :return: Returns a DataFrame of active SDG Labels
+    :rtype: DataFrame
+    """
+
+    sdglabels = api.search({'portal_type':"BatchLabel"})
+    cols = [
+        'title',
+        'description',
+    ]
+
+    sdglabel_dict = {}
+    for i in sdglabel_dict:
+        sdglabel_dict[i] = []
+
+    for i in sdglabels:
+        if api.get_workflow_status_of(i) == 'active':
+            sdglabel = api.get_object(i)
+            sdglabel_dict['title'].append(sdglabel.title) #Required
+            sdglabel_dict['description'].append(sdglabel.description or '')
+
+    return pd.DataFrame(sdglabel_dict)[cols]
+
+def get_sdgs_as_df():
+    """
+    :return: Returns a DataFrame of active SDG
+    :rtype: DataFrame
+    """
+
+    sdgs = api.search({'portal_type':"Batch"})
+    cols = [
+        'title',
+        'description'
+        'BatchID',
+        'Client',
+        'ClientBatchID',
+        'BatchLabels',
+        'SDGDate',
+        'SDGTime',
+        'ReportContact',
+        'ProjectContact',
+        'SamplerContact',
+        'GrowerContact',
+        'COC'
+    ]
+
+    sdg_dict = {}
+    for i in sdg_dict:
+        sdg_dict[i] = []
+
+    for i in sdgs:
+        if api.get_workflow_status_of(i) == 'active':
+            sdg = api.get_object(i)
+            sdg_dict['title'].append(sdg.title) #Required
+            sdg_dict['description'].append(sdg.description or '')
+            sdg_dict['BatchID'].append(sdg.BatchID or '')
+            sdg_dict['Client'].append(sdg.get_relationship('BatchClient') or '') ##
+            sdg_dict['ClientBatchID'].append(sdg.ClientBatchID or '')
+            sdg_dict['BatchLabels'].append(sdg.getLabelNames() or '') ##
+            sdg_dict['SDGDate'].append(sdg.SDGDate or '')
+            sdg_dict['SDGTime'].append(sdg.SDGTime or '')
+            sdg_dict['ReportContact'].append(sdg.ReportContact or '')
+            sdg_dict['ProjectContact'].append(sdg.get_relationship('SDGProjectContact') or '') ##
+            sdg_dict['SamplerContact'].append(sdg.get_relationship('SDGSamplerContact') or '') ##
+            sdg_dict['GrowerContact'].append(sdg.get_relationship('SDGGrowerContact') or '') ##
+            sdg_dict['COC'].append(sdg.COC or '') ##
+
+    return pd.DataFrame(sdg_dict)[cols]
