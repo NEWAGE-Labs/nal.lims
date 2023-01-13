f3dd344468d3a40caef51821c1d789b44da8a0c7
Paul VanderWeele <faytro@hotmail.com>
2023-01-13 14:58:15 -0500

    Add logging to analyses extract

--- src/nal/lims/datamigration/extracts.py
+++ src/nal/lims/datamigration/extracts.py
@@ -955,6 +955,7 @@ def get_analyses_as_df():
     """
 
     analyses = api.search({'portal_type':"Analysis"})
+    print("Analyses are: {}".format(analyses))
     cols = [
         'sid',
         'Keyword',
@@ -977,8 +978,9 @@ def get_analyses_as_df():
         analysis_dict[i] = []
 
     for i in analyses:
-        if api.get_workflow_status_of(i) not in ['inactive','invalid','cancelled','rejected','retracted','unassigned','dispatched']:
+        if api.get_workflow_status_of(i) not in ['inactive','invalid','cancelled','rejected','retracted']:
             analysis = api.get_object(i)
+            print("Found Analysis: {} of {}".format(api.get_id(analysis),api.get_id(analysis.ac_parent)))
             analysis_dict['sid'].append(api.get_id(analysis.ac_parent)) #Required
             analysis_dict['Keyword'].append(analysis.Keyword or '')
             analysis_dict['method'].append(analysis.getMethod() or '')
